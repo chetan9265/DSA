70f7af31b1b805b3d4cccb83cd973d6e2324dee5
chetan9265 <chetanrajput9265@gmail.com>
2025-02-05 10:34:00 +0530

    Update

---
+++ Task-Days-4/LeapYear.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	for(int i=2000; i<=3000; i+=4){
+		cout<<i<<endl;
+	}
+	return 0;
+}
---
+++ Task-Days-4/Oneton.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=1;
+    int n;
+    cout<<"Enter a number :";
+    cin>>n;
+    
+    while(i<=n){
+    	cout<<i;
+    	i++;
+	}
+    
+    
+return 0;
+}
