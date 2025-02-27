8dc4b413afe580f48df50e8f6ccf5064b2e96931
chetan9265 <chetanrajput9265@gmail.com>
2025-02-27 09:55:49 +0530

    Update

--- function/4.cpp
+++ function/4.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
 using namespace std;
-void fact(int n){
-	for()
+int fact(int n){
+	int sum=1;
+	for(int i=1; i<=n; i++){
+	sum*=i;
+	}
+	return sum;
 }
  int main(){
- 	
- 	
+ 	int cube=fact(4);
+ 	cout<<cube;
  }
